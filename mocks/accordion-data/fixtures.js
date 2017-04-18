99277007e746f8c9c2ed8b06029f98650a138cdd
Ronaldo Medina <fr.medinah@gmail.com>
2017-04-18 13:37:17 +0200

    Build index.html page demo for accordion component.

--- mocks/accordion-data/fixtures.js
+++ mocks/accordion-data/fixtures.js
@@ -2,29 +2,29 @@
 
 module.exports = {
 	'accordion-data-1': {
-		title: '1. Accordion loaded with ajax GET /rest/api/accordion/data-1',
+		title: 'Sample 1. Accordion with content loaded with ajax request (GET /rest/api/accordion/data-1)',
 		color: 'red',
 		animated: 'false',
 		sections: [
 			{
 				title: 'Dynamic section 1',
-				content: 'Dynamic content of section 1'
+				content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed porta arcu. Integer lacinia et lorem at finibus. Donec luctus blandit nibh, nec aliquet risus dapibus sit amet. Sed sed fringilla lectus, a commodo tellus. Nullam non tincidunt elit, vitae laoreet ipsum. Nulla maximus semper libero quis accumsan. Mauris cursus aliquam ipsum. Donec iaculis in elit id placerat. Aliquam malesuada turpis erat, at placerat nulla finibus sed. Vestibulum ante est, venenatis eu massa nec, fringilla interdum tellus. Donec accumsan nisl scelerisque aliquam eleifend. Ut ornare efficitur nunc, vel euismod eros scelerisque ac. Suspendisse nulla augue, ultrices sit amet semper rhoncus, fringilla pellentesque mauris. In auctor quam nec consequat congue. Proin bibendum enim vitae bibendum sagittis.'
 			},
 			{
 				title: 'Dynamic section 2',
-				content: 'Dynamic content of section 2'
+				content: 'Cras posuere sed lacus at bibendum. Suspendisse finibus magna ac elit pellentesque porta. Cras varius quam magna, a fringilla nisl finibus ut. Aenean dignissim libero pulvinar convallis varius. Ut cursus dictum maximus. Proin luctus, massa ultricies tristique sollicitudin, lorem justo commodo metus, vulputate auctor neque nisl ut urna. Proin et aliquet augue. Donec condimentum sit amet odio in scelerisque. Vestibulum ac vehicula urna. Quisque id blandit mauris. Vivamus suscipit bibendum velit at sagittis. Aliquam magna purus, porta eget elit quis, fermentum lacinia lectus. Suspendisse et lorem eget lacus lobortis euismod ac nec nunc.'
 			},
 			{
 				title: 'Dynamic section 3',
-				content: 'Dynamic content of section 3'
+				content: 'Sed eu tortor in sapien dictum pharetra. Phasellus ac felis ac tortor tempor mattis. Donec sodales nisi nec nisi porttitor semper. Vivamus est diam, tempor sed neque at, pulvinar condimentum diam. Morbi at feugiat nunc. Cras nec metus molestie, blandit est at, viverra augue. Aenean interdum aliquet dolor, interdum vehicula eros condimentum non.'
 			},
 			{
 				title: 'Dynamic section 4',
-				content: 'Dynamic content of section 4'
+				content: 'Duis et elementum risus. Maecenas et tellus lacus. Ut at enim molestie purus auctor tempor vel sed justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean a fermentum velit, ut dapibus dolor. Nulla sapien leo, placerat eu ultricies ut, bibendum vitae nulla. Maecenas volutpat porta sapien non porttitor. Phasellus pellentesque diam ut metus egestas, vel aliquam risus sodales. Vestibulum purus massa, aliquet ac commodo ac, volutpat eu tortor. Aliquam nec venenatis felis, vitae efficitur magna. Suspendisse accumsan, augue ut tempus efficitur, urna nisl faucibus tellus, id vulputate mauris quam eget nunc. Nam fermentum consectetur massa, non tristique quam malesuada non. In a dapibus nisi, eget dictum nisi.'
 			},
 			{
 				title: 'Dynamic section 5',
-				content: 'Dynamic content of section 5'
+				content: 'Proin lacinia eget est vel congue. Sed id venenatis ipsum, eget bibendum leo. Etiam tincidunt nibh et feugiat aliquam. Etiam vehicula nulla non aliquam dignissim. Donec eu dignissim diam. Nulla facilisi. Aliquam vitae semper sapien.'
 			}
 		]
 	}
